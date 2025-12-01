98994b49e6478c05683eaeff50c65f5685820a39
RekCutPleh <tizianosilvestri123@gmail.com>
2025-12-01 02:03:49 -0300

    Update authMiddleware
    Update authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,13 +11,19 @@ const protegerRuta = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decodificado = jwt.verify(token, process.env.JWT_SECRET);
-      req.usuario = await User.findById(decodificado.id).select('-contraseña');
+      req.usuario = await User.findById(decodificado.id).select('-password'); // Corregido '-contraseña' a '-password'
+
+      // Cabeceras para prevenir cache
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+      res.setHeader('Pragma', 'no-cache');
+      res.setHeader('Expires', '0');
+
       next();
     } catch (error) {
       return res.status(401).json({ mensaje: 'Token inválido' });
     }
   } else {
-    return res.status(401).json({ mensaje: 'No autorizado, sin token' });
+    return res.status(401).json({ mensaje: 'No autorizado, no hay token' });
   }
 };
 
